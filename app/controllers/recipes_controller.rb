b08dfaa5c9ea839f374837285d43137ca5066b2a
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-11-21 17:26:05 +0100

    Add basic index and show logic

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,12 @@
 class RecipesController < ApplicationController
+  def index
+    @recipes = current_user.recipes
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+    return if @recipe.is_public? || @recipe.user == current_user
+
+    redirect_to root_path
+  end
 end
