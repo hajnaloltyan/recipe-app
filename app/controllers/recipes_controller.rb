4aad0f8d7b94b9eaa75795974bc5946f1100d839
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-11-21 17:44:55 +0100

    Create basic views for recipe pages

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,7 @@
 class RecipesController < ApplicationController
   def index
-    @recipes = current_user.recipes
+    # @recipes = current_user.recipes
+    @recipes = Recipe.all
   end
 
   def show
@@ -8,5 +9,9 @@ class RecipesController < ApplicationController
     return if @recipe.is_public? || @recipe.user == current_user
 
     redirect_to root_path
+  end
+
+  def public_recipes
+    @public_recipes = Recipe.where(is_public: true)
   end
 end
