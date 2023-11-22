2db9c84b1a799b903308578352f5d97f37366c9c
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-11-22 20:47:02 +0100

    Fix signup and login actions, add name to the signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, alert: exception.message
+    redirect_to root_url
   end
 
   protected
