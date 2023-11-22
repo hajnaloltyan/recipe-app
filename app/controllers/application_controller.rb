e98e277aa39c5f83a5a326e4b22d7aaf1df5e6d7
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-11-22 20:54:32 +0100

    Fix linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  rescue_from CanCan::AccessDenied do |exception|
+  rescue_from CanCan::AccessDenied do |_exception|
     redirect_to root_url
   end
 
