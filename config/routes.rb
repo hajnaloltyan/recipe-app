82b786c4b4db31fb7884199159365cdd0958652c
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-11-22 20:05:08 +0100

    Add some style to the pages, and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  root 'recipes#index'
+  root 'recipes#public_recipes'
   resources :recipes
 
   devise_for :users
