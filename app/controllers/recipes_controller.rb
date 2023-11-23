28a0c868c670642534e981dc5ef4ae14d8544152
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-11-23 20:15:11 +0100

    Create form to add recipe and create method in controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -19,4 +19,19 @@ class RecipesController < ApplicationController
   def new
     @recipe = Recipe.new
   end
+
+  def create
+    @recipe = current_user.recipes.build(recipe_params)
+    if @recipe.save
+      redirect_to @recipe, notice: 'Recipe was successfully added.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :is_public)
+  end
 end
