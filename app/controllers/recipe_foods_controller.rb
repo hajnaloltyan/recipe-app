baf783aef8f72f0f3b7586ba5fc31195908d6694
Cristian Villa <c.villavirgen@gmail.com>
2023-11-22 22:39:20 -0600

    Fix linters

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,11 +1,10 @@
 class RecipeFoodsController < ApplicationController
-    def index
-      @recipe = Recipe.find(params[:recipe_id])
-      @recipe_foods = @recipe.recipe_foods
-    end
-  
-    def show
-      @recipe_food = RecipeFood.find(params[:id])
-    end
+  def index
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_foods = @recipe.recipe_foods
   end
-  
+
+  def show
+    @recipe_food = RecipeFood.find(params[:id])
+  end
+end
