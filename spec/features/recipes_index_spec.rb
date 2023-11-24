4d51ce02f6c7f03f22e7ff211d39625680ebb658
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-11-24 16:18:14 +0100

    Add tests for Recipes index page

--- spec/features/recipes_index_spec.rb
+++ spec/features/recipes_index_spec.rb
@@ -9,10 +9,24 @@ RSpec.describe 'Recipes Index Page', type: :system do
     sign_in user
   end
 
-  it 'displays all recipes for user one' do
+  it 'shows title of the page' do
     visit '/recipes'
     expect(page).to have_content 'My Recipes'
+  end
+
+  it 'displays all recipes for user one' do
+    visit '/recipes'
     expect(page).to have_content 'Tomato Pasta'
+  end
+
+  it 'show option to add new recipe' do
+    visit '/recipes'
     expect(page).to have_link('New Recipe', href: new_recipe_path)
+  end
+
+  it 'shows recipe details after clicking on recipe' do
+    visit '/recipes'
+    click_on 'Tomato Pasta'
+    expect(page).to have_content 'Tomato Pasta'
   end
 end
