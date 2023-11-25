6b20d4685681ce9d05da8c87947a6884112d1d22
Cristian Villa <c.villavirgen@gmail.com>
2023-11-24 21:05:04 -0600

    Fix linters

--- spec/features/recipe_foods/list_recipe_foods_spec.rb
+++ spec/features/recipe_foods/list_recipe_foods_spec.rb
@@ -4,18 +4,18 @@ RSpec.feature 'List Recipe Foods', type: :feature do
   scenario 'User views recipe foods and navigates to add a new ingredient' do
     # Create a single user and food
     user = FactoryBot.create(:user)
-    food = FactoryBot.create(:food, user: user)
-    recipe = FactoryBot.create(:recipe, user: user)
+    food = FactoryBot.create(:food, user:)
+    recipe = FactoryBot.create(:recipe, user:)
 
     # Create several recipe_food instances using the same user and food
-    FactoryBot.create_list(:recipe_food, 3, recipe: recipe, food: food)
+    FactoryBot.create_list(:recipe_food, 3, recipe:, food:)
 
     login_as(user, scope: :user)
 
     visit recipe_recipe_foods_path(recipe)
 
-    expect(page).to have_content('Recipe Foods for ' + recipe.name)
-    expect(page).to have_selector('tr', count: 4)  # Including header row
+    expect(page).to have_content("Recipe Foods for #{recipe.name}")
+    expect(page).to have_selector('tr', count: 4) # Including header row
 
     click_link 'Add Ingredient'
     expect(current_path).to eq(new_recipe_recipe_food_path(recipe))
